5cb6ef6bb701d1eec7797164fe58552aaf64cb90
Alan Hummel <ahummel@abim.org>
2023-05-03 10:45:06 -0400

    Work in progress on solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TypedFormExerciseComponent } from './typed-form-exercise/typed-form-exercise.component';
 import { IntroComponent } from './intro/intro.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { IntroComponent } from './intro/intro.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/typed-form-exercise/typed-form-exercise.component.ts
+++ src/app/typed-form-exercise/typed-form-exercise.component.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core';
 import { CustomerService } from '../customer.service';
 import { AddCustomerCommand } from '../add-customer-command';
 import { finalize } from 'rxjs';
-import { FormControl } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
-interface CustomerForm {
+interface ICustomerForm {
   firstName: FormControl<string>;
   lastName: FormControl<string>;
   address1: FormControl<string>;
-  address2: FormControl<string|null>;
+  address2: FormControl<string | null>;
   city: FormControl<string>;
   state: FormControl<string>;
   postalCode: FormControl<string>;
   emailAddress: FormControl<string>;
+  phone: FormControl<string>;
+  phoneExt: FormControl<number | null>
 }
 
 @Component({
@@ -23,9 +25,12 @@ interface CustomerForm {
 export class TypedFormExerciseComponent {
 
   public saving: boolean = false;
+  public customerForm: FormGroup<ICustomerForm>;
 
-  constructor(private _customerService: CustomerService) {
-
+  constructor(
+    private _customerService: CustomerService,
+    formBuilder: FormBuilder) {
+      this.customerForm = this.buildForm(formBuilder);
   }
 
   public submitForm(): void {
@@ -34,6 +39,27 @@ export class TypedFormExerciseComponent {
       .addCustomer(new AddCustomerCommand())
       .pipe(finalize(() => this.saving = false))
       .subscribe((id: number) => window.alert(id));
+  }
+
+  private buildForm(formBuilder: FormBuilder): FormGroup<ICustomerForm> {
+    return formBuilder.group<ICustomerForm>({
+        firstName: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+        lastName: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+        address1: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+        address2: new FormControl<string | null>(null),
+        city: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+        state: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+        postalCode: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+        emailAddress: new FormControl<string>('', { nonNullable: true, validators: [Validators.required, Validators.email] }),
+        phone: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+        phoneExt: new FormControl<number | null>(null)
+      }
+    );
+  }
+
+  private getAddCustomerCommand(): AddCustomerCommand {
+    const command = new AddCustomerCommand();
+    return command;
   }
 
 }
